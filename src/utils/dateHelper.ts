9e3658d81b199d765964391172687f6df7259761
Sajal Prosun <prosunsajal123@gmail.com>
2026-08-28 12:00:00 +0600

    feat: add dateHelper utils with formatting and diff helpers

---
+++ src/utils/dateHelper.ts
@@ -0,0 +1,3 @@
+export const formatDate = (d: Date): string => d.toISOString().split('T')[0];
+export const diffDays = (a: Date, b: Date): number => Math.ceil(Math.abs(a.getTime()-b.getTime())/86400000);
+export const addDays = (d: Date, n: number): Date => { const x=new Date(d); x.setDate(x.getDate()+n); return x; };
