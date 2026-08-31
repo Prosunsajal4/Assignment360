12cb393e7a16cf87ef5766c9ff87557cf363275f
Sajal Prosun <prosunsajal123@gmail.com>
2026-08-31 12:00:00 +0600

    feat: add lightweight logger utility

---
+++ src/utils/logger.ts
@@ -0,0 +1,1 @@
+export const logger = { info: (m: string, d?: unknown) => console.log('[INFO] '+m, d ?? ''), error: (m: string, e?: unknown) => console.error('[ERROR] '+m, e ?? ''), warn: (m: string, d?: unknown) => console.warn('[WARN] '+m, d ?? '') };
