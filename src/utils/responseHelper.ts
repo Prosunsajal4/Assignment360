5eff2c75472857aa747984bcd5e6d666c0b5dbee
Sajal Prosun <prosunsajal123@gmail.com>
2026-09-01 12:00:00 +0600

    feat: add API response helper

---
+++ src/utils/responseHelper.ts
@@ -0,0 +1,2 @@
+export const success = <T>(data: T, message='Success') => ({ success: true, message, data });
+export const failure = (message='Error', errors: unknown=null) => ({ success: false, message, errors });
