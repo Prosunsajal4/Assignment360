11de3b3a9a56cae99ea6a1309013e1c0f7ce5188
Sajal Prosun <prosunsajal123@gmail.com>
2026-09-02 12:00:00 +0600

    feat: add common validator helpers

---
+++ src/utils/validators.ts
@@ -0,0 +1,2 @@
+export const isEmail = (s: string): boolean => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(s);
+export const isStrongPassword = (s: string): boolean => s.length>=8 && /[A-Z]/.test(s) && /[0-9]/.test(s);
