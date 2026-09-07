e83e09a55846e875a747411caae3660cf900c335
Sajal Prosun <prosunsajal123@gmail.com>
2026-09-07 12:00:00 +0600

    feat: add weekend check to dateHelper

--- src/utils/dateHelper.ts
+++ src/utils/dateHelper.ts
@@ -1,3 +1,4 @@
 export const formatDate = (d: Date): string => d.toISOString().split('T')[0];
 export const diffDays = (a: Date, b: Date): number => Math.ceil(Math.abs(a.getTime()-b.getTime())/86400000);
 export const addDays = (d: Date, n: number): Date => { const x=new Date(d); x.setDate(x.getDate()+n); return x; };
+export const isWeekend = (d: Date): boolean => [0,6].includes(d.getDay());
