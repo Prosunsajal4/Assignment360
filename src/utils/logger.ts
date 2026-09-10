5ccf9828aad09c734197041d142ccbe4b3f02eb3
Sajal Prosun <prosunsajal123@gmail.com>
2026-09-10 19:32:49 +0600

    feat: add debug logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,1 +1,2 @@
 export const logger = { info: (m: string, d?: unknown) => console.log('[INFO] '+m, d ?? ''), error: (m: string, e?: unknown) => console.error('[ERROR] '+m, e ?? ''), warn: (m: string, d?: unknown) => console.warn('[WARN] '+m, d ?? '') };
+export const debug = (m: string, d?: unknown) => console.debug('[DEBUG] '+m, d ?? '');
